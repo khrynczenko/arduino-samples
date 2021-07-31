7233fb0b7d79a38cab034a5bef18f5989197d0e5
Krzysztof Hrynczenko <jeniopy@gmail.com>
2021-07-31 09:42:44 +0200

    make pwm steered diode go back and forth

--- pwm-diode-signal/pwm-diode-signal.ino
+++ pwm-diode-signal/pwm-diode-signal.ino
@@ -2,6 +2,7 @@
 
 byte fill = 0;
 int change = 5;
+bool increasing_is_active = true;
 
 
 void setup() {
@@ -10,7 +11,18 @@ void setup() {
 
 void loop() {
   analogWrite(DIODE_PIN, fill);
-  fill = fill + 1;
+  if (increasing_is_active && fill > 240) {
+    increasing_is_active = false;
+  }
+  if (!increasing_is_active && fill < 20){
+    increasing_is_active = true;
+  }
+  if (increasing_is_active) {
+      fill = fill + 10;
+  } else {
+    fill = fill - 10;
+  }
 
-  delay(1000);
+
+  delay(100);
 }
